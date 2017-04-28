f6d18ef56dcd8752f6e28bf26f95efd75415aa96
s.zenkov <s.zenkov@corp.nekki.ru>
2017-04-28 18:48:07 +0300

    preparings for new stop-at-the-wall mechanism

--- Assets/PlayerController2D.cs
+++ Assets/PlayerController2D.cs
@@ -22,6 +22,8 @@ public class PlayerController2D : MonoBehaviour {
     private Rigidbody2D _rigidbody;
     private BoxCollider2D _collider;
 
+    private const float INPUT_CLAMPING = 0.99f;
+
     void Awake() {
         _rigidbody = GetComponent<Rigidbody2D>();
         _collider = GetComponent<BoxCollider2D>();
@@ -37,22 +39,36 @@ public class PlayerController2D : MonoBehaviour {
         float vInput = Input.GetAxis(axisYName);
         float hInput = Input.GetAxis(axisXName);
 
+        //Makes axis inputs discrete and makes input zero right after releasing axis buttons
+        vInput = Mathf.Clamp01(Mathf.Abs(vInput) - INPUT_CLAMPING) * Mathf.Sign(vInput);
+        hInput = Mathf.Clamp01(Mathf.Abs(hInput) - INPUT_CLAMPING) * Mathf.Sign(hInput);
+
         verticalText.text = vInput.ToString();
         horizontalText.text = hInput.ToString();
+
         if (vInput != 0 || hInput != 0) {
-            animator.SetBool("moving", true);
-            _rigidbody.MovePosition(transform.localPosition += new Vector3(hInput * speedX, vInput * speedY));
-            //transform.localPosition += new Vector3(hInput * speedX, vInput * speedY);
+            //animator.SetBool("moving", true);
+            var currentSpeedX = hInput != 0 ? speedX * Mathf.Sign(hInput) : 0;
+            var currentSpeedY = vInput != 0 ? speedY * Mathf.Sign(vInput) : 0;
+
+            var moveDirection = new Vector3(currentSpeedX, currentSpeedY);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, moveDirection, _collider.size.x / 2);
+            if (hit != null) {
+                _rigidbody.velocity = new Vector2(0f, 0f);
+            }
+            _rigidbody.velocity = new Vector2(currentSpeedX, currentSpeedY);
+            
         }
         else
         {
-
+            _rigidbody.velocity = new Vector2(0, 0);
         }
 	}
 
     public void OnTriggerEnter2D(Collider2D other) {
         float contactPosition = other.bounds.center.x - other.bounds.size.x / 2;
         float myWidth = _collider.bounds.size.x;
-        float posX = contactPosition - myWidth / 2f;
+        float posX = contactPosition + myWidth / 2f;
+        //_rigidbody.MovePosition(new Vector3(posX, transform.localPosition.y, transform.localPosition.z));
     }
 }
